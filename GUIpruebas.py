8865930b379c111b6166cd9af76f4ff7d8924853
mewtie <mrsurinen@gmail.com>
2018-03-23 03:59:06 -0500

    Primer avance de la class ventana

---
+++ GUIpruebas.py
@@ -0,0 +1,35 @@
+from mainModuleV1 import *
+from tkinter import *
+
+class menu():
+    def __init__(self):
+        self.objWindow = Tk()
+        self.objWindow.title("Organizador de Series")
+        self.objWindow.geometry("420x280")
+        self.fileImgTitle = PhotoImage(file = "files/imgTitle.gif")
+        self.imgTitle = Label(self.objWindow, image = self.fileImgTitle)
+        self.imgTitle.pack()
+
+        self.mainMenu()
+
+        self.objWindow.mainloop()
+    def mainMenu(self):
+        self.addSerieBottom = Button(self.objWindow, text = "Agregar Serie", command = self.redimencionar)
+        self.addSerieBottom.pack()
+        self.removeSerieBottom = Button(self.objWindow, text = "Remover Serie", command = self.redimencionar1)
+        self.removeSerieBottom.pack()
+        self.toListSerieBottom = Button(self.objWindow, text = "Listar Series")
+        self.toListSerieBottom.pack()
+        self.loadDataBaseBottom = Button(self.objWindow, text = "Cargar una base de datos")
+        self.loadDataBaseBottom.pack()
+
+
+    def redimencionar(self):
+        self.objWindow.geometry("600x420")
+
+    def redimencionar1(self):
+        self.objWindow.geometry("420x280")
+
+
+
+objMenu = menu()
